8a89755cee26f9d5e22983e1c038df3e525b540a
Emanuel-diaz <emanuel4008894@gmail.com>
2022-09-29 20:27:06 -0300

    practica de js

---
+++ js/HolaMundo-Variables.js
@@ -0,0 +1,37 @@
+console.log("Hola mundo");
+/* definicion de variables/ asignacion de variables */
+let numero = 5; //el signo de = es el operador de asignacion
+console.log(numero);
+let numero2 = 8;
+console.log(numero2);
+numero2 = numero; //re asignando un valor a una variable
+console.log(numero2);
+let anio = 2022;
+let anio_pasado = 2021;
+let nombreApellido = "Emanuel Gonzalo Diaz";
+//constantes
+/* const palabra = "hola";
+console.log(palabra);
+palabra = "chau";
+var numero3 = 10; */
+//tipo de datos
+//tipo de dato string
+//defino un string vacio
+let stringVacio = "";
+
+let numberString = "8";
+let numberString2 = "10";
+console.log(numberString);
+console.log(numberString2);
+
+let num = 10;
+let number = 20;
+console.log(num + number);
+document.write("<h4> La suma de los numeros es</h4>");
+document.write(number + num);
+
+//tipo de datos booleano
+let verdadero = true;
+console.log(verdadero);
+let falso = false;
+console.log(!verdadero);
